f9a2e8f4af1ac1d3072c55c5a8b375315d679c22
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-05-01 14:40:05 +0100

    check

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import SideNav from "./Components/SideNav";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
+import "./App.css";
 function App() {
   return (
     <>
-      <div class="h-screen w-screen rounded-lg flex">
+      <div class="lg:h-screen w-screen rounded-3xl shadow-[0px_38px_200px_rgba(0,0,0,0.1)] flex">
         <SideNav />
         <div class="flex-1">
           <Header />
