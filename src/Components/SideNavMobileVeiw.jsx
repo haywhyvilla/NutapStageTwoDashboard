52d7a77cd1c5e5cae17f8fef8175b5f1550dc275
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-05-02 00:21:00 +0100

    1 billion

--- src/Components/SideNavMobileVeiw.jsx
+++ src/Components/SideNavMobileVeiw.jsx
@@ -14,7 +14,6 @@ import { ImCancelCircle } from "react-icons/im";
 
 const SideNavMobileVeiw = () => {
   const [open, setOpen] = useState(false);
-
   const handleClick = () => {
     setOpen(!open);
   };
